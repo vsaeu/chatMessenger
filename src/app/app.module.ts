de6c44eb09f502e7099f5bf7ba0890fdecbf296e
Rinat Madreiter <umfragmail@gmx.de>
2022-06-18 01:31:21 +0200

    first draft of channel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelComponent } from './channel/channel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelComponent
   ],
   imports: [
     BrowserModule,

995f37f76728c9ee643a8430eedee2e29f8d5aa8
Christian Pfaff <madadaskar@googlemail.com>
2022-06-22 19:24:23 +0200

    thread component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,16 +20,13 @@ import { ChatComponent } from './chat/chat.component';
 import { FormsModule } from '@angular/forms';
 import { DialogCreateChannelComponent } from './dialog-create-channel/dialog-create-channel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { ThreadPersonComponent } from './thread-person/thread-person.component';
-=======
 import {MatButtonModule} from '@angular/material/button';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
->>>>>>> 49fae880788666f23684fedbc2b4e626ed15ddae
 
 @NgModule({
   declarations: [
